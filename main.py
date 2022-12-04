0a47e2d2c2698ee10a4122ec60cd76834b09f10a
Programmistochka <programmistochka@list.ru>
2022-12-04 18:54:13 +1200

    Add def Heroes

--- main.py
+++ main.py
@@ -11,20 +11,53 @@ class SuperHero:
         response = requests.get(request_url, headers = headers)
         data = response.json()
      
-        print(data)
-        #for hero in data:
-        #    print (hero['id'], hero['name'])
         list_heroes=[]
         for hero in data:
-            #print (hero['id'], hero['name'])
             list_heroes.append({'id': hero['id'],
                                 'name': hero['name'],
                                 'intelligence':  hero['powerstats']['intelligence']})
+        return list_heroes
+
+    def find_id_hero_by_name(self, name_hero):
+        list_heroes = self._get_data()
         print(list_heroes)
+        for hero in list_heroes:
+            if hero['name'] == name_hero:
+                id_hero = hero['id']
+                break
+        return id_hero
+    
+    def add_intalligence_by_name(self, name_hero):
+        list_heroes = self._get_data()
+        print(list_heroes)
+        for hero in list_heroes:
+            if hero['name'] == name_hero:
+                intelligence = hero['intelligence']
+                break
+        return intelligence
+
+class Heroes:
+
+    def __init__(self, id_hero, name, intelligence=0):
+        self.id = id_hero
+        self.name = name
+        self.intelligence = intelligence
+
+    def __lt__(self, other):
+        if not isinstance(other, Heroes):
+            print(f'Ошибка. {other} не относится к классу Heroes')
+        else:
+            return self.intelligence < other.intelligence
+
+    def __str__(self):
+        res = f'{self.name} id({self.id}): intelligence = {self.intelligence} '
+        return res
+
+    
 
     
 
 if __name__ == '__main__':
   sh = SuperHero()
-  sh._get_data()
-  #sh.list_all_heroes()
+  Hulk = Heroes(sh.find_id_hero_by_name('Hulk'),'Hulk', sh.add_intalligence_by_name('Hulk'))
+  print(Hulk)
